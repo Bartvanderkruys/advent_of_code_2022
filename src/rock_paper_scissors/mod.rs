1c5e6d49e62da844ab1d66a0e044d65da8ea03f7
bartvanderkruijs <bartvanderkruys@gmail.com>
2022-12-04 13:01:10 +0100

    Implement Day 2 part 2

--- src/rock_paper_scissors/mod.rs
+++ src/rock_paper_scissors/mod.rs
@@ -5,6 +5,10 @@ const WIN: i8 = 6;
 const DRAW: i8 = 3;
 const LOSS: i8 = 0;
 
+// hands:
+// 1: rock
+// 2: paper
+// 3: scissors
 fn get_hand(x: char) -> i8 {
     match x {
         'A' | 'X' => 1,
@@ -17,22 +21,44 @@ fn get_hand(x: char) -> i8 {
 fn match_result(player: i8, opponent: i8) -> i8 {
     match player {
         1 => match opponent {
-            1 => DRAW,
             2 => LOSS,
             3 => WIN,
-            _ => 0,
+            _ => DRAW,
         },
         2 => match opponent {
             1 => WIN,
-            2 => DRAW,
             3 => LOSS,
-            _ => 0,
+            _ => DRAW,
         },
         3 => match opponent {
             1 => LOSS,
             2 => WIN,
-            3 => DRAW,
-            _ => 0,
+            _ => DRAW,
+        },
+        _ => 0,
+    }
+}
+
+// strategy:
+// 1: lose
+// 2: draw
+// 3: win
+fn determine_player_hand(strategy: i8, opponent: i8) -> i8 {
+    match opponent {
+        1 => match strategy {
+            1 => 3,
+            3 => 2,
+            _ => 1,
+        },
+        2 => match strategy {
+            1 => 1,
+            3 => 3,
+            _ => 2,
+        },
+        3 => match strategy {
+            1 => 2,
+            3 => 1,
+            _ => 3,
         },
         _ => 0,
     }
@@ -49,7 +75,9 @@ pub fn solve() {
         .lines()
         .map(|line| {
             let opponent = get_hand(line.chars().nth(0).unwrap());
-            let player = get_hand(line.chars().nth(2).unwrap());
+            let strategy = get_hand(line.chars().nth(2).unwrap());
+
+            let player = determine_player_hand(strategy, opponent);
 
             calculate_points_for_round(player, opponent)
         })
