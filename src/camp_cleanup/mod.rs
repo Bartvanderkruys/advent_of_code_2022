11296874d2403d1b72450f32f343337b4272b055
bartvanderkruijs <bartvanderkruys@gmail.com>
2022-12-05 22:47:11 +0100

    Implement day 4 part 2

--- src/camp_cleanup/mod.rs
+++ src/camp_cleanup/mod.rs
@@ -11,7 +11,7 @@ fn convert_line_to_ranges(line: &str) -> Vec<RangeInclusive<i32>> {
         .collect()
 }
 
-fn has_overlap<'a>(line: &'a &str) -> bool {
+fn has_complete_overlap<'a>(line: &'a &str) -> bool {
     let ranges = convert_line_to_ranges(line);
 
     let range_a = ranges.first().unwrap();
@@ -28,10 +28,32 @@ fn has_overlap<'a>(line: &'a &str) -> bool {
     false
 }
 
+fn has_partial_overlap<'a>(line: &'a &str) -> bool {
+    let ranges = convert_line_to_ranges(line);
+
+    let range_a = ranges.first().unwrap();
+    let range_b = ranges.last().unwrap();
+
+    if range_a.contains(&range_b.start()) || range_a.contains(&range_b.end()) {
+        return true;
+    }
+
+    if range_b.contains(&range_a.start()) || range_b.contains(&range_a.end()) {
+        return true;
+    }
+
+    false
+}
+
 pub fn solve() {
     let contents = fs::read_to_string("src/camp_cleanup/input.txt").unwrap();
 
-    let solution = contents.lines().filter(has_overlap).count();
-
-    println!("Number of complete overlaps: {}", solution);
+    println!(
+        "Number of complete overlaps: {}",
+        contents.lines().filter(has_complete_overlap).count()
+    );
+    println!(
+        "Number of partial overlaps: {}",
+        contents.lines().filter(has_partial_overlap).count()
+    );
 }
