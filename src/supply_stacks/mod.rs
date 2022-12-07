55619d2ad4519f761bbcc44cf72221fe1b712d50
bartvanderkruijs <bartvanderkruys@gmail.com>
2022-12-07 19:46:40 +0100

    Implement day 5 part 1

--- src/supply_stacks/mod.rs
+++ src/supply_stacks/mod.rs
@@ -1,14 +1,37 @@
 use std::fs;
 
-type Stacks = Vec<Vec<char>>;
+type Stack = Vec<char>;
+type Instruction = (usize, usize, usize);
 
-fn get_stacks(contents: &String) -> Stacks {
-    let mut stacks: Stacks = vec![];
-
-    let stack_base_line = contents
+fn get_stack_base_line(contents: &String) -> usize {
+    contents
         .lines()
         .position(|x| x.chars().nth(1) == Some('1'))
-        .unwrap();
+        .unwrap()
+}
+
+fn parse_instruction(string: &str) -> usize {
+    string.parse::<usize>().unwrap()
+}
+
+fn get_instructions(contents: &String, stack_base_line: usize) -> Vec<Instruction> {
+    contents
+        .lines()
+        .skip(stack_base_line + 2)
+        .map(|line| {
+            let mut split = line.split(" ");
+
+            (
+                parse_instruction(&split.nth(1).unwrap()),
+                parse_instruction(&split.nth(1).unwrap()),
+                parse_instruction(&split.nth(1).unwrap()),
+            )
+        })
+        .collect()
+}
+
+fn get_stacks(contents: &String, stack_base_line: usize) -> Vec<Stack> {
+    let mut stacks: Vec<Stack> = vec![];
 
     let number_of_stacks = contents
         .lines()
@@ -34,8 +57,42 @@ fn get_stacks(contents: &String) -> Stacks {
     stacks
 }
 
+fn drain_blocks(
+    stacks: &mut Vec<Stack>,
+    origin_index: usize,
+    number_of_blocks: usize,
+) -> Vec<char> {
+    stacks
+        .get_mut(origin_index)
+        .unwrap()
+        .drain(0..number_of_blocks)
+        .collect()
+}
+
+fn execute_instructions(stacks: &mut Vec<Stack>, instructions: &Vec<Instruction>) {
+    for instruction in instructions.iter() {
+        let blocks = drain_blocks(stacks, instruction.1 - 1, instruction.0);
+
+        for block in blocks {
+            stacks.get_mut(instruction.2 - 1).unwrap().insert(0, block);
+        }
+    }
+}
+
 pub fn solve() {
     let contents = fs::read_to_string("src/supply_stacks/input.txt").unwrap();
 
-    let stacks: Stacks = get_stacks(&contents);
+    let stack_base_line = get_stack_base_line(&contents);
+    let mut stacks = get_stacks(&contents, stack_base_line);
+    let instructions = get_instructions(&contents, stack_base_line);
+
+    execute_instructions(&mut stacks, &instructions);
+
+    println!(
+        "{}",
+        stacks
+            .iter()
+            .map(|stack| { stack.first().unwrap() })
+            .collect::<String>()
+    )
 }
