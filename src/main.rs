7f2960624580546949553031fe359ec037076732
bartvanderkruijs <bartvanderkruys@gmail.com>
2022-12-07 20:05:31 +0100

    Prepend mods with day number.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #[allow(dead_code)]
-mod calorie_counting;
-mod camp_cleanup;
-mod rock_paper_scissors;
-mod rucksack_reorganization;
-mod supply_stacks;
+mod day_1_calorie_counting;
+mod day_2_rock_paper_scissors;
+mod day_3_rucksack_reorganization;
+mod day_4_camp_cleanup;
+mod day_5_supply_stacks;
 
 const DAY: i8 = 5;
 
 fn main() {
     match DAY {
-        1 => calorie_counting::solve(),
-        2 => rock_paper_scissors::solve(),
-        3 => rucksack_reorganization::solve(),
-        4 => camp_cleanup::solve(),
-        5 => supply_stacks::solve(),
+        1 => day_1_calorie_counting::solve(),
+        2 => day_2_rock_paper_scissors::solve(),
+        3 => day_3_rucksack_reorganization::solve(),
+        4 => day_4_camp_cleanup::solve(),
+        5 => day_5_supply_stacks::solve(),
         _ => (),
     }
 }
